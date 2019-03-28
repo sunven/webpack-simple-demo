1961101df68d3ee4934d1abc5720e9e83c84883c
seven <604164227@qq.com>
2019-03-28 18:25:47 +0800

    init project

---
+++ src/Child.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>{{abc}}</div>
+    <!-- <input type="text" v-model="abc"> -->
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: ["abc"]
+};
+</script>
+
